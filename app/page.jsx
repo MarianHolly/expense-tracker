09115cf140885298448ed2a71f60c2b5852aafb8
Stupel <hellomariandev@gmail.com>
2024-04-08 14:31:40 +0200

    .

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import ExpenseSummary from "@/components/expense-summary";
 
 export default function Home() {
   return (
-    <main className="container grid grid-cols-1 grid-rows-2 lg:grid-cols-2 gap-y-3 gap-x-3 py-6 min-h-screen bg-slate-50">
+    <main className="container grid grid-cols-1 grid-rows-2 lg:grid-cols-2 gap-y-3 gap-x-3 p-3 min-h-screen bg-slate-50">
       <ExpenseSummary />
       <ExpenseHistory />
       <ExpenseForm />
