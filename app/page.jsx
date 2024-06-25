1cf473c269b8b892eb284b9eeb1ed98870d27fea
Stupel <hellomariandev@gmail.com>
2024-06-25 12:56:04 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,30 +3,40 @@
 import ExpenseForm from "@/components/expense-form";
 import ExpenseHistory from "@/components/expense-history";
 import ExpenseSummary from "@/components/expense-summary";
+import { useState } from "react";
 
 export default function Home() {
   // transactions
-  const transactions = [{ id: 1, title: "Book", amount: -24 }];
+  const initialState = [{ id: 1, title: "Book", amount: -24 }];
+  const [transactions, setTransactions] = useState(initialState);
 
   // adding transaction (income or expense)
-  function addTransaction() {}
+  function addTransaction(newTransaction) {
+    setTransactions([...transactions, newTransaction]);
+  }
   // deleting transaction
-  function deleteTransaction() {}
+  function deleteTransaction(id) {
+    setTransactions(transactions.filter((transaction) => transaction.id !== id));
+  }
   // reset tracker
-  function reset() {}
+  function reset() {
+    setTransactions([]);
+  }
 
-  // balance
-  const balance = 100;
-  // incomes
-  const incomes = 170;
-  // expences
-  const expences = -70;
+  // Calculate balance, incomes, and expenses
+  const balance = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+  const incomes = transactions
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+  const expenses = transactions
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
 
   return (
     <main className="container grid grid-cols-1 lg:grid-cols-2 grid-rows-1 lg:grid-rows-2 gap-y-3 gap-x-3 p-3 min-h-screen">
       <ExpenseHistory transactions={transactions} deleteTransaction={deleteTransaction} />
       <ExpenseForm addTransaction={addTransaction} />
-      <ExpenseSummary balance={balance} incomes={incomes} expences={expences} reset={reset} />
+      <ExpenseSummary balance={balance} incomes={incomes} expenses={expenses} reset={reset} />
     </main>
   );
 }
