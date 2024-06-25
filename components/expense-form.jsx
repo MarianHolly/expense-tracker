eefda05753ef2e878a4744ac42a0d15d7775c4b5
Stupel <hellomariandev@gmail.com>
2024-06-25 12:56:02 +0200

    Update expense-form.jsx

--- components/expense-form.jsx
+++ components/expense-form.jsx
@@ -4,55 +4,51 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import { Input } from "./ui/input";
-import { Calendar } from "./ui/calendar";
 import { Button } from "./ui/button";
 
 export default function ExpenseForm({ addTransaction }) {
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState(0);
-  const [date, setDate] = useState(new Date());
 
   function onSubmit(e) {
     e.preventDefault();
+    if (title === "" || amount === 0) {
+      alert("Please enter both title and amount");
+      return;
+    }
 
     const newTransaction = {
       id: uuidv4(),
       title,
-      amount,
-      date,
+      amount: parseFloat(amount),
     };
+    addTransaction(newTransaction);
+    console.log(newTransaction);
     setTitle("");
     setAmount(0);
-
-    console.log(newTransaction);
-    addTransaction(newTransaction);
   }
 
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <h1 className="text-xl font-extrabold uppercase text-slate-900 opacity-25 my-3">Form</h1>
-
-      <form
-        onSubmit={onSubmit}
-        className="flex flex-col gap-y-3 items-center"
-      >
+      <form onSubmit={onSubmit} className="flex flex-col gap-y-3 items-center">
         <Input
           type="text"
-          placeholder="Enter title for expense or income"
-          className="w-96 text-md"
           value={title}
+          placeholder="Enter title for expense or income"
           onChange={(e) => setTitle(e.target.value)}
+          className="w-96 text-md"
         />
-
         <Input
           type="number"
-          placeholder="Enter amount"
-          className="w-44 text-md"
           value={amount}
+          placeholder="Enter amount"
           onChange={(e) => setAmount(e.target.value)}
+          className="w-44 text-md"
         />
- 
-        <Button size="lg">Add transaction</Button>
+        <Button size="lg" type="submit">
+          Add transaction
+        </Button>
       </form>
     </div>
   );
