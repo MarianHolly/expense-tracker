be5fd155ec5bcfdf76259eb00684b29995a79e62
Stupel <hellomariandev@gmail.com>
2024-06-25 14:10:11 +0200

    Update index.jsx

--- context/index.jsx
+++ context/index.jsx
@@ -1,41 +1,27 @@
 "use client";
 
-import { createContext, useReducer, useState } from "react";
-
-// Reducer function
-function trackerReducer(state, action) {
-  switch (action.type) {
-    case "ADD_TRANSACTION":
-      return [...state, action.payload];
-    case "DELETE_TRANSACTION":
-      return state.filter((transaction) => transaction.id !== action.payload);
-    case "RESET":
-      return [];
-    default:
-      return state;
-  }
-}
-
-const initialState = [{ id: 1, title: "Book", amount: -24 }];
+import { createContext } from "react";
+import useLocalStorage from "@/hooks/use-local-storage";
 
 export const TrackerContext = createContext(null);
 
 export const TrackerProvider = ({ children }) => {
-  const [transactions, dispatch] = useReducer(trackerReducer, initialState);
+  const initialState = [{ id: 1, title: "Book", amount: -24 }];
 
-  // Actions
-  const addTransaction = (newTransaction) => {
-    dispatch({ type: "ADD_TRANSACTION", payload: newTransaction });
-  };
+  const [transactions, setTransactions] = useLocalStorage("expense-tracker", initialState);
 
-  const deleteTransaction = (id) => {
-    dispatch({ type: "DELETE_TRANSACTION", payload: id });
-  };
-
-  const reset = () => {
-    dispatch({ type: "RESET" });
-  };
-
+  // adding transaction (income or expense)
+  function addTransaction(newTransaction) {
+    setTransactions([...transactions, newTransaction]);
+  }
+  // deleting transaction
+  function deleteTransaction(id) {
+    setTransactions(transactions.filter((transaction) => transaction.id !== id));
+  }
+  // reset tracker
+  function reset() {
+    setTransactions([]);
+  }
   // Calculate balance, incomes, and expenses
   const balance = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
   const incomes = transactions
